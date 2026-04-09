7eb5f74bd78316bb23d0ca7fd2b76480e767baac
nachitom06 <i.agustin.monzon06@gmail.com>
2026-04-09 15:03:36 -0300

    Add files via upload

---
+++ Actividades-clase-3/ModeladoDeTDAs/src/bloque2/DistribucionTickets.java
@@ -0,0 +1,13 @@
+import Interface.ColaPrioridadTDA;
+import Implementacion.ColaPrioridadEstatica;
+
+public class DistribucionTickets {
+    public void main(String[] args){
+        ColaPrioridadTDA colaConPriori= new ColaPrioridadEstatica();
+
+        colaConPriori.InicializarColaPrioridad();
+        colaConPriori.AcolarPrioridad(1, 999);
+        colaConPriori.AcolarPrioridad(2, 0);
+        System.out.println(colaConPriori.Primero());
+    }
+}
