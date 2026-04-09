cad98d6398e2e866b2f3be3be43740873786d0e8
nachitom06 <i.agustin.monzon06@gmail.com>
2026-04-09 16:01:12 -0300

    Add files via upload

---
+++ Actividades-clase-3/ModeladoDeTDAs/src/bloque2/BuffetFacultad.java
@@ -0,0 +1,15 @@
+import Interface.ColaTDA;
+import Implementacion.ColaEstatica;
+
+public class BuffetFacultad {
+    public void main(String[] args){
+        ColaTDA cola= new ColaEstatica();
+        cola.InicializarCola();
+        cola.Acolar(1); // Usas una cola normal para que nadie tenga prioridad sobre nadie sino que son atendidos en base a cuando llegaron.
+        cola.Acolar(2);
+        cola.Acolar(3);
+        cola.Acolar(4);
+        cola.Acolar(5);
+        System.out.println(cola.Primero());
+    }
+}
