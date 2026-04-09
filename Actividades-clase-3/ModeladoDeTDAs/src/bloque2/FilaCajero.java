eb04326cb4d548d8071b2a02215fcf34f3c45dbb
Rodrigo Galarce <rgalarce@uade.edu.ar>
2026-04-09 16:06:28 -0300

    Finaliza FilaCajero

--- Actividades-clase-3/ModeladoDeTDAs/src/bloque2/FilaCajero.java
+++ Actividades-clase-3/ModeladoDeTDAs/src/bloque2/FilaCajero.java
@@ -1,66 +1,18 @@
-import Implementacion.Estrategia_1;
-import Interfaces.PilaTDA;
-
-public class Persona {
-
-    String nombre;
-    int prioridad;
-
-    public Persona(String nombre, int prioridad) {
-        this.nombre = nombre;
-        this.prioridad = prioridad;
-    }
-
-}
-
-public interface ColaTDA {
-    void inicializarCola();
-    void acolar();
-    void desacolar();
-
-
-
-}
-
-public class Cola implements ColaTDA {
-
-    ArrayList<Persona> cola;
-
-    public Cola() {
-
-    }
-
-    @java.lang.Override
-    public void inicializarCola() {
-        cola = new ArrayList<Persona>();
-    }
-
-    @java.lang.Override
-    public void acolar(Persona persona) {
-        cola.add(persona);
-    }
-}
+package bloque2;
+import Implementacion.ColaPrioridadEstatica;
+import Interfaces.ColaPrioridadTDA;
 
 public class FilaCajero {
-
-//PilaTDA filaCajero = new FilaCajero;
-    Cola colaCajero = new Cola();
+    
+    ColaPrioridadTDA filaCajero = new ColaPrioridadEstatica() {
+        
+    };
 
     public FilaCajero() {
-        colaCajero.InicializarCola();
-        colaCajero.agregarPersona(new Persona("Juan", 1))
-        colaCajero.agregarPersona(new Persona("Juan", 2))
-        colaCajero.agregarPersona(new Persona("Juan", 2))
-
-    }
-
-    public void agregarPersona(Persona persona) {
-
-    }
-
-
-    public OrdenarPrioridad() {
-
+        filaCajero.InicializarColaPrioridad();
+        filaCajero.AcolarPrioridad(17, 35);
+        filaCajero.AcolarPrioridad(1, 45);
+        filaCajero.AcolarPrioridad(5, 50);
 
     }
 }
