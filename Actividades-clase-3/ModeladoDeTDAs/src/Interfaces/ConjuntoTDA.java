c839178c0f55cc9ed77507d7a960d756817041ca
Rodrigo Galarce <rgalarce@uade.edu.ar>
2026-04-09 16:37:45 -0300

    Agrega interfaz e implementacion de Conjuntos

---
+++ Actividades-clase-3/ModeladoDeTDAs/src/Interfaces/ConjuntoTDA.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public class ConjuntoTDA {
+    void InicializarConjunto();
+    void Agregar(int x);
+    void Sacar(int x);
+    boolean ConjuntoVacio();
+    boolean Pertenece(int x);
+    int Elegir();
+}
