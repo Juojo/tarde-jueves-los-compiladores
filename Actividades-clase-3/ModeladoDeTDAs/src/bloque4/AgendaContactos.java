6ac10e4f138b7f2a4da7eee032538481b80a66fd
nachitom06 <i.agustin.monzon06@gmail.com>
2026-04-15 22:15:36 -0300

    Update AgendaContactos.java

--- Actividades-clase-3/ModeladoDeTDAs/src/bloque4/AgendaContactos.java
+++ Actividades-clase-3/ModeladoDeTDAs/src/bloque4/AgendaContactos.java
@@ -1,5 +1,5 @@
 import Implementacion.DiccionarioSimpleEstaticoString;
-import Interface.DiccionarioSimpleStringTDA;
+import Interfaces.DiccionarioSimpleStringTDA;
 
 public class AgendaContactos {
     public static void main(String[] args){
