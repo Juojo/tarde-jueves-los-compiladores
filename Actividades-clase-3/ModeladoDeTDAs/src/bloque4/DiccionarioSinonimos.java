0037cecb21a47659be8c40ef375d751047fd75a0
nachitom06 <i.agustin.monzon06@gmail.com>
2026-04-15 22:15:55 -0300

    Fix import statement for DiccionarioMultipleStringAmbosTDA

--- Actividades-clase-3/ModeladoDeTDAs/src/bloque4/DiccionarioSinonimos.java
+++ Actividades-clase-3/ModeladoDeTDAs/src/bloque4/DiccionarioSinonimos.java
@@ -1,6 +1,6 @@
 
 import Implementacion.DiccionarioMultipleEstaticoStringAmbos;
-import Interface.DiccionarioMultipleStringAmbosTDA;
+import Interfaces.DiccionarioMultipleStringAmbosTDA;
 
 public class DiccionarioSinonimos {
     public static void main(String[] args) {
