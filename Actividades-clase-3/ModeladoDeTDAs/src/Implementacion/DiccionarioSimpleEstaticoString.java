e26db95e775673141ac0fe0d0fd17bec11f9a33f
nachitom06 <i.agustin.monzon06@gmail.com>
2026-04-15 22:13:28 -0300

    Add files via upload

---
+++ Actividades-clase-3/ModeladoDeTDAs/src/Implementacion/DiccionarioSimpleEstaticoString.java
@@ -0,0 +1,59 @@
+package Implementacion;
+
+import Interface.DiccionarioSimpleStringTDA;
+
+public class DiccionarioSimpleEstaticoString implements DiccionarioSimpleStringTDA {
+
+    private static final int MAX = 100;
+    private String[] claves;
+    private int[] valores;
+    private int cantidad;
+
+    @Override
+    public void InicializarDiccionario() {
+        claves = new String[MAX];
+        valores = new int[MAX];
+        cantidad = 0;
+    }
+
+    @Override
+    public void Agregar(String clave, int valor) {
+        int i = 0;
+
+        while (i < cantidad && claves[i] != clave) {
+            i++;
+        }
+
+        if (i < cantidad) {
+            valores[i] = valor; // actualiza si la clave ya existe
+        } else if (cantidad < MAX) {
+            claves[cantidad] = clave;
+            valores[cantidad] = valor;
+            cantidad++;
+        }
+    }
+
+    @Override
+    public void Eliminar(String clave) {
+        int i = 0;
+
+        while (i < cantidad && !(claves[i].equals(clave))) {
+            i++;
+        }
+
+        if (i < cantidad) {
+            claves[i] = claves[cantidad - 1];
+            valores[i] = valores[cantidad - 1];
+            cantidad--;
+        }
+    }
+
+    @Override
+    public String[] Claves() {
+        String[] aux = new String[cantidad];
+        for (int i = 0; i < cantidad; i++) {
+            aux[i] = claves[i];
+        }
+        return aux;
+    }
+}
